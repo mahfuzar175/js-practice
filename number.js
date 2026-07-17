be69c193188a8b94baed5b5881a6b44ccef12917
mahfuzar175 <mahfuzar175@gmail.com>
2026-07-17 23:25:20 +0600

    learn about number

---
+++ number.js
@@ -0,0 +1,23 @@
+var oPrice = 20;
+var chocolate = 0.5;
+var aPrice = 20;
+var mPrice = parseInt ("30");
+var jPrice = parseInt ("djifhdf");
+var tPrice = parseFloat ("40.5");
+
+console.log(typeof oPrice);
+console.log(typeof aPrice);
+console.log(typeof mPrice);
+console.log(typeof jPrice);
+console.log(typeof tPrice);
+console.log(oPrice + aPrice);
+
+
+var fst = 0.2;
+var snd = .10;
+var to = fst + snd;
+console.log (to.toFixed(2));
+
+
+const num = 50/0;
+console.log(num);
