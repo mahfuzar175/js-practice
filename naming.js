02d49c893339fff17903fc76e946412d7c5826d7
mahfuzar175 <mahfuzar175@gmail.com>
2026-07-17 21:40:23 +0600

    naming learning

---
+++ naming.js
@@ -0,0 +1,26 @@
+// 1. no keyword in var
+
+var false = 45;
+var var = 10;
+
+//  2. no space support
+var is happy= true;
+var isHappy = true; 
+
+// 3. no quote
+var "tarek" = "mui";
+
+// 4. cant start a number; after letter number is allowed
+var 3mo = 45;
+var t2 = 10;
+
+// 5. name us case sensitive
+addess
+Address
+
+// 6. log variable name
+var my current address = "chakborkat";
+var my_current_addess = "chakbo" //snake case
+var myCurrentAddress = "chakborkat"; //camel case
+var MyCurrentAddress = "chakborkat"; // pascal case
+
--- types.js
+++ types.js
@@ -5,3 +5,21 @@ console.log(name)
 
 var addess = "nilkhet"
 console.log(addess);
+
+
+// boolean type
+var isRich= true;
+console.log(isRich);
+
+var pass = false;
+console.log(pass);
+console.log(typeof pass);
+console.log(typeof name);
+
+// number
+var tk = 20;
+console.log(tk);
+console.log(typeof tk);
+
+
+
