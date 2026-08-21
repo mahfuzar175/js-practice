e6db0839ab16acabfd5d663cf003aae01361f414
mahfuzar175 <mahfuzar175@gmail.com>
2026-08-21 18:29:19 +0600

    new folder created

---
+++ Js conditionals/compare.js
@@ -0,0 +1,13 @@
+/**
+ * compare
+ * bigger: >
+ * lesser: <
+ * equal: ==
+ * grater than or equal: >=
+ * less than or equal: <=
+ * not equal: !=
+ * 
+ * ----------------------
+ * and: &&
+ * or: || 
+ */
---
+++ Variable/shorthand.js
@@ -0,0 +1,10 @@
+var price = 35;
+// price = price + 10;
+price += 10;
+console.log(price);
+
+
+var f = 25000;
+// f = f-1000;
+f -= 1000;
+console.log(f);
