43d9791227fbed29dd561e5ffb8c00632e221eb3
mahfuzar175 <mahfuzar175@gmail.com>
2026-08-21 19:13:47 +0600

    learn multiple condition

---
+++ Js conditionals/multiple.js
@@ -0,0 +1,16 @@
+const salary = 22000;
+const isBCS = true;
+const h = 61;
+
+// if(salary > 20000 && h > 62){
+//     console.log("biye")
+// }
+// else{
+//     console.log("not biye")
+// }
+if(salary > 20000 || h > 62){
+    console.log("biye")
+}
+else{
+    console.log("not biye")
+}
