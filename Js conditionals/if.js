e1bf52af5cbc96e2489cf7bd5236eadf1891ec81
mahfuzar175 <mahfuzar175@gmail.com>
2026-08-21 18:55:15 +0600

    learn if else

---
+++ Js conditionals/if.js
@@ -0,0 +1,33 @@
+/**
+ *================ structure ==============
+ if(condition){
+ }
+ */
+
+ if(3 > 18 ){
+    console.log("ami biye korbo");
+ }
+
+ var B = 100;
+ if(B < 300){
+    console.log("khabo");
+ }
+
+
+ /**
+  * if(con){
+  *  if trure
+  * }
+  * else{
+  *  if flase
+  * }
+  */
+
+
+ var W = 40;
+ if(W < 20){
+    console.log("i will buy")
+ }
+ else{
+    console.log("not buy")
+ }
